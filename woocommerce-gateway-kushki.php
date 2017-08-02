b80b61e8f0762bec167a863e2d761033eaae1b16
Patricio Moreano <pmoreanoj@gmail.com>
2017-08-02 12:44:53 -0500

    [KSD-212] Setting correct currency in cajita

--- woocommerce-gateway-kushki.php
+++ woocommerce-gateway-kushki.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Kushki - WooCommerce Payment Gateway
  * Plugin URI:        https://www.kushkipagos.com/
  * Description:       Kushki payment gateway for WooCommerce.
- * Version:           2.0.2
+ * Version:           2.0.5
  * Author:            Kushki
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
